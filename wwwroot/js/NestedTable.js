372f16db0548844dc463a19f7c93d67bc0e56487
agla venetia <agla.venetia@gmail.com>
2024-01-30 17:28:31 +0530

    30-Jan-2024

--- wwwroot/js/NestedTable.js
+++ wwwroot/js/NestedTable.js
@@ -4,10 +4,12 @@
     {
         $(this).closest("tr").after("<tr><td></td><td colspan = '999'>" + $(this).next().html() + "</td></tr>");
         $(this).attr("src", "/images/minus.png");
+        $(this).next("div").children().remove();
     });
 
     //Assign Click event to Minus Image.
     $("body").on("click", "img[src*='minus.png']", function () {
+        $(this).next("div").prepend($(this).closest("tr").next().children().eq(1).html());
         $(this).attr("src", "/images/plus.png");
-    $(this).closest("tr").next().remove();
+        $(this).closest("tr").next().remove();
         });
